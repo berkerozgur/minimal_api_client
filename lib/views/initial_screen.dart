523c9f3175642033c9c48febcc904d6983b77cd2
Berker Ozgur <89974136+berkerozgur@users.noreply.github.com>
2025-07-03 19:00:21 +0300

    feat: refactor InitialScreen to use StatefulWidget
    - Changed InitialScreen from StatelessWidget to StatefulWidget
    - Added loading state management and response handling
    - Updated UI to reflect loading state and display responses

--- lib/views/initial_screen.dart
+++ lib/views/initial_screen.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
 
-class InitialScreen extends StatelessWidget {
+class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  var _isLoading = false;
+  var _response = 'Enter the URL and click Send to get a response';
+
+  void _sendRequest() async {
+    // TODO: Validate the url
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      // TODO: Replace this delay with actual logic as needed.
+      await Future.delayed(const Duration(seconds: 1));
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _response = 'Error: $e';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +35,29 @@ class InitialScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Minimal API Client'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter URL',
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                ElevatedButton(onPressed: _sendRequest, child: Text('Send')),
+              ],
+            ),
+            SizedBox(height: 8),
+            _isLoading ? CircularProgressIndicator() : Text(_response),
+          ],
+        ),
       ),
     );
   }
